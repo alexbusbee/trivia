24c97ead9b928186f58e3ac78f5b3056e388a91b
Alex Busbee <alex.busbee@gmail.com>
2017-02-21 00:32:34 -0600

    Shuffle JSON results

--- src/components/trivia/trivia.component.ts
+++ src/components/trivia/trivia.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'trivia',
@@ -9,6 +10,17 @@ export class TriviaComponent {
   items: FirebaseListObservable<any[]>;  
  
   constructor(af: AngularFire) {
-    this.items = af.database.list('/us-capitals');
+    this.items = <FirebaseListObservable<any>> af.database
+      .list('/us-capitals')
+      .map(items => {
+        function shuffle(a) {
+            for (let i = a.length; i; i--) {
+                let j = Math.floor(Math.random() * i);
+                [a[i - 1], a[j]] = [a[j], a[i - 1]];
+            }
+        }
+        shuffle(items);
+        return items;
+      })
   }
 }
